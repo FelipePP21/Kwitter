9f14a17d4d2103d522802cd2f1eef37f6fe4087c
Aline Takai <99260756+alineathomaz@users.noreply.github.com>
2023-10-11 11:55:30 -0300

    Update kwitter_page.js

--- kwitter_page.js
+++ kwitter_page.js
@@ -10,9 +10,8 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig)
 
-user_name = localStorage.getItem("user_name")
-
-room_name = localStorage.getItem("room_name")
+user_name = localStorage.getItem("user_name");
+room_name = localStorage.getItem("room_name");
 
 function send()
 {
@@ -43,13 +42,17 @@ function getData()
         message - messageData['message'];
         like - messageData['like']*/
         name = message_data['name'];
-	    message = message_data['message'];
+	message = message_data['message'];
         like = message_data['like'];
-        nameWithTag = "<h4> "+ name +"<img class='user_tick' src= 'tick.png'></h4>";
+        /*nameWithTag = "<h4> "+ name +"<img class='user_tick' src= 'tick.png'></h4>";
         likeButton ="<button class='btn btn-warning' id="+firebaseMessageId+" value="+like+"onclick='updateLike(this.id)'>";
         spanWithTag = "<span class='glyphicon glyphicon-thumbs-up'>Like: "+ like +"</span></button><hr>";
-
-        row = nameWithTag + messageWithTag + likeButton + spanWithTag;
+        row = nameWithTag + messageWithTag + likeButton + spanWithTag;*/
+	name_with_tag = "<h4> "+ name +"<img class='user_tick' src='tick.png'></h4>";
+        message_with_tag = "<h4 class='message_h4'>" + message + "</h4>";
+        like_button ="<button class='btn btn-warning' id="+firebase_message_id+" value="+ like+" onclick='updateLike(this.id)'>";
+        span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Curtidas: "+ like +"</span></button><hr>";
+        row = name_with_tag + message_with_tag +like_button + span_with_tag;
         document.getElementById("output").innerHTML += row;
 
       } 
@@ -59,21 +62,28 @@ function getData()
 
 getData();
 
-function updateLike(messageId)
+//function updateLike(messageId)
+function updateLike(message_id)
 {
-  console.log("botão like pressionado - " + messageId);
-  button_id = messageId;
+  /*console.log("botão like pressionado - " + messageId);
+  button_id = messageId;*/
+  console.log("clicou no botão curtir - " + message_id);
+  button_id = message_id;
   likes = document.getElementById(button_id).value;
-  updatedLikes - Number(likes) + 1;
-  console.log(updatedLikes);
+  /*updatedLikes - Number(likes) + 1;
+  console.log(updatedLikes);*/
+  updated_likes = Number(likes) + 1;
+  console.log(updated_likes);
 
-  firebase.database().ref(roomName).child(messageId).update ({
-    like : updatedLikes
+  /*firebase.database().ref(roomName).child(messageId).update ({
+    like : updatedLikes*/
+  firebase.database().ref(room_name).child(message_id).update({
+    like : updated_likes
   });
 }
 
 function logout(){
-  localStorage.removeItem("userName");
-  localStorage.removeItem("roomName");
+  localStorage.removeItem("user_name");
+  localStorage.removeItem("room_name");
   window.location.replace("index.html");
 }
